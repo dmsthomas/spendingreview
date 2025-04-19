94a215ca1a08bd4d86d23fd6959eeeaa760c1dbc
dmsthomas <dmthomas@protonmail.com>
2025-04-19 20:08:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,12 @@ with tab_tax:
                     header_ph = container.empty()
                     slider_val = container.slider(
                         label=r['name'],
-                        min_value=int(r.get('min_pct', r.get('min_change', 0))),
+                        min_value=int(r.get('min_change', r.get('min_pct', 0))), max_value=int(r.get('max_change', r.get('max_pct', 0))),
+                        value=tax_changes[r['name']],
+                        key=key,
+                        label_visibility="collapsed",
+                        help=r.get('note',''),
+                    ))),
                         max_value=int(r.get('max_pct', r.get('max_change', 0))),
                         value=int(spend_changes.get(r['name'], 0) * 100),
                         key=key,
